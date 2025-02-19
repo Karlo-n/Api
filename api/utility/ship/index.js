b83f7746788d1b99be6bbe005d3fb39102f02b47
Karlo-n <yolorivera.5678@gmail.com>
2025-02-18 22:23:03 -0600

    Delete api/utility/ship directory

--- api/utility/ship/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json({ message: "🚀 API de Ship funcionando correctamente!" });
-});
-
-module.exports = router;
