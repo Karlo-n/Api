8a7ed8c293aacec7db5b0709bc7ab64d21aa0bb4
Karlo-n <yolorivera.5678@gmail.com>
2025-02-21 23:10:55 -0600

    Update index.js

--- api/utility/ranked/index.js
+++ api/utility/ranked/index.js
@@ -68,7 +68,7 @@ router.get("/", async (req, res) => {
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         // Dibujar avatar correctamente en un círculo sin bordes extraños
-        const avatarX = -5;
+        const avatarX =  5;
         const avatarY = -2;
         const avatarWidth = 350; // Expandido solo en X
         const avatarHeight = 160; // Mantiene la altura
